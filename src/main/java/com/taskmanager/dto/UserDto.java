28df05ed2c2b67315241f58f2d4b6977a410a78c
mustafa.durkaya <mustafadrkya@icloud.com>
2025-08-07 14:33:32 +0300

    -feat- dto

---
+++ src/main/java/com/taskmanager/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.taskmanager.dto;
+
+import com.taskmanager.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Role role;
+}
