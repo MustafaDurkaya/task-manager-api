80b1c593d774bc846aa66a575332e80ffc9d506f
mustafa.durkaya <mustafadrkya@icloud.com>
2025-08-07 10:28:17 +0300

    -feat- Define permission sets for each Role

--- src/main/java/com/taskmanager/enums/Role.java
+++ src/main/java/com/taskmanager/enums/Role.java
@@ -1,6 +1,19 @@
 package com.taskmanager.enums;
 
+import java.util.Set;
+
 public enum Role {
-    USER,
-    ADMIN
+    ADMIN(Set.of("USER_READ", "USER_WRITE", "ADMIN_PANEL")),
+    USER(Set.of("USER_READ", "USER_WRITE")),
+    GUEST(Set.of("PUBLIC_READ"));
+
+    private final Set<String> permissions;
+
+    Role(Set<String> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<String> getPermissions() {
+        return permissions;
+    }
 }
